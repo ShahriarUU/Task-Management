f603c89a8be44f4825e6d25d2088e5bed325074f
Shahriar <Shahriar.asraf@yahoo.com>
2024-06-30 13:57:35 +0600

    chore :add  jwt token method

--- backend/src/models/userModel.js
+++ backend/src/models/userModel.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import validator from "validator";
+import bcrypt from "bcryptjs";
 
 const userSchema = mongoose.Schema({
 
@@ -37,6 +38,51 @@ const userSchema = mongoose.Schema({
     },
 
 })
+
+
+//password hashing
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) {
+        return next();
+    }
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+})
+
+
+//compare  password
+userSchema.methods.isPasswordCorrect = async function (password) {
+    return bcrypt.compare(password, this.password);
+};
+
+//Generate Access Token 
+userSchema.methods.generateAccessToken = async function () {
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+        }
+    );
+};
+
+
+//Generate Refresh Token
+
+userSchema.methods.generateRefreshToken = async function () {
+    return jwt.sign(
+        {
+            _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+        }
+    );
+};
 
 
 const User = mongoose.model("User", userSchema);
